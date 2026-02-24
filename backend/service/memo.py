7b7d8ec8bcbceb83ff56590236d445e9ec64d109
LinSrHang <1023948565@qq.com>
2026-02-25 01:13:25 +0800

    feat(service): 新增备忘录服务类提供增删改查功能
    - 实现 create_memo、update_memo、delete_memo 和 get_memo_list 方法
    - 支持按提醒日期和完成状态筛选，并包含分页功能
    - 新增内部方法用于计算指定日期的起始和结束时间

---
+++ backend/service/memo.py
@@ -0,0 +1,95 @@
+from datetime import datetime, timezone
+
+from sqlalchemy.sql._typing import _ColumnExpressionArgument
+
+from model.memo_store import Memo
+from sqlite.engine import SQLiteEngine
+
+
+class MemoService:
+    engine: SQLiteEngine
+
+    def __init__(self, engine: SQLiteEngine):
+        self.engine = engine
+
+    async def create_memo(
+        self,
+        title: str,
+        alert_at: datetime,
+        is_urgent: bool,
+        content: str = "",
+    ):
+        memo = Memo(
+            title=title,
+            content=content,
+            alert_at=alert_at,
+            is_urgent=is_urgent,
+        )
+        await self.engine.create(memo)
+
+    async def update_memo(
+        self,
+        id: int,
+        title: str | None = None,
+        content: str | None = None,
+        alert_at: datetime | None = None,
+        is_urgent: bool | None = None,
+        done: bool | None = None,
+    ):
+        memo = Memo(
+            id=id,
+            title=title,
+            content=content,
+            alert_at=alert_at,
+            is_urgent=is_urgent,
+            done=done,
+        )
+        await self.engine.update(memo)
+
+    async def delete_memo(self, id: int):
+        await self.engine.delete(Memo, Memo.id == id)
+
+    async def get_memo_list(
+        self,
+        alert_day: datetime | None = None,
+        done: bool | None = None,
+        page: int = 1,
+        page_size: int = 10,
+    ) -> list[Memo]:
+        query: list[_ColumnExpressionArgument[bool] | bool] = []
+        if done is not None:
+            query.append(Memo.done == done)
+        if alert_day:
+            start, end = self.__get_alert_day_start_and_end(alert_day)
+            query.append(Memo.alert_at >= start)
+            query.append(Memo.alert_at <= end)
+
+        offset = (page - 1) * page_size
+        limit = page_size
+        memos = await self.engine.query(Memo, *query, offset=offset, limit=limit)
+        return memos
+
+    def __get_alert_day_start_and_end(
+        self, alert_day: datetime
+    ) -> tuple[datetime, datetime]:
+        start = datetime(
+            year=alert_day.year,
+            month=alert_day.month,
+            day=alert_day.day,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=timezone.utc,
+        )
+        end = datetime(
+            year=alert_day.year,
+            month=alert_day.month,
+            day=alert_day.day,
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999,
+            tzinfo=timezone.utc,
+        )
+        return start, end
